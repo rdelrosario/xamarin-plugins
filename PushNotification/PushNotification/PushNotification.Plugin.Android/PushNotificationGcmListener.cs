dc1d2bf2de1aa0ead8f8713411f6db4d8a2c14bd
Rendy Del Rosario <rendy@crossgeeks.com>
2016-09-03 23:57:01 -0400

    Changes to fix issue #84

--- PushNotification/PushNotification/PushNotification.Plugin.Android/PushNotificationGcmListener.cs
+++ PushNotification/PushNotification/PushNotification.Plugin.Android/PushNotificationGcmListener.cs
@@ -249,8 +249,9 @@ namespace PushNotification.Plugin
 
             // Create a PendingIntent; we're only using one PendingIntent (ID = 0):
             const int pendingIntentId = 0;
-            PendingIntent resultPendingIntent = PendingIntent.GetActivity(context, pendingIntentId, resultIntent, PendingIntentFlags.OneShot);
-
+            var currentMillis = (long)DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            PendingIntent resultPendingIntent = PendingIntent.GetActivity(context, pendingIntentId, resultIntent, PendingIntentFlags.OneShot | PendingIntentFlags.UpdateCurrent );
+            resultPendingIntent.SetAction($"currentMillis");
             // Build the notification
             builder = new NotificationCompat.Builder(context)
                       .SetAutoCancel(true) // dismiss the notification from the notification area when the user clicks on it
